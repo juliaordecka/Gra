96d44442d7c5bf926ce835d709462bace16621fa
juliaordecka <juliaordecka@gmail.com>
2024-05-19 03:51:59 +0200

    modyfikacja memory - dopasowywanie trojek zamiast  par + jeden dodatkowy obrazek

--- Gra/Form2.cs
+++ Gra/Form2.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Formats.Asn1.AsnWriter;
 using Timer = System.Windows.Forms.Timer;
 
 namespace Gra
@@ -18,14 +13,20 @@ namespace Gra
         bool allowClick = false;
         Random rnd = new Random();
         PictureBox firstGuess;
+        PictureBox secondGuess;
+        PictureBox thirdGuess;
         Timer clickTimer = new Timer();
+        Timer specialTimer = new Timer();
         int time = 60;
         Timer timer = new Timer { Interval = 1000 };
+        PictureBox specialPictureBox;
 
         public Form2(Form1 parent)
         {
             InitializeComponent();
             this.parent = parent;
+            specialPictureBox = new PictureBox();
+
         }
 
         private PictureBox[] pictureBoxes
@@ -35,9 +36,11 @@ namespace Gra
 
         private void Form2_Load(object sender, EventArgs e)
         {
-
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Click += clickImage;
+            }
         }
-
 
         private static IEnumerable<Image> images
         {
@@ -49,18 +52,14 @@ namespace Gra
                     Properties.Resources._2,
                     Properties.Resources._3,
                     Properties.Resources._4,
-                    Properties.Resources._5,
-                    Properties.Resources._6,
-                    Properties.Resources._7,
-                    Properties.Resources._8
-
+                    Properties.Resources._5
                 };
             }
-
         }
 
         private void startGameTimer()
         {
+            
             timer.Start();
             timer.Tick += delegate
             {
@@ -69,12 +68,11 @@ namespace Gra
                 {
                     timer.Stop();
                     clickTimer.Stop();
+                    specialTimer.Stop();
                     MessageBox.Show("Koniec czasu");
-                    //ZAPIS WYNIKU
                     parent.SCORE = 0;
                     parent.RODZAJ = "Memory";
                     parent.Zapis_do_CSV();
-                    //////////////////
                     ResetImages();
                 }
                 var ssTime = TimeSpan.FromSeconds(time);
@@ -88,9 +86,11 @@ namespace Gra
             {
                 pic.Tag = null;
                 pic.Visible = true;
+                
             }
-            HideImages();
             setRandomImages();
+            HideImages();
+            
             time = 60;
             timer.Start();
         }
@@ -101,6 +101,7 @@ namespace Gra
             {
                 pic.Image = Properties.Resources.x;
             }
+            specialPictureBox.Image = Properties.Resources.x;
         }
 
         private PictureBox getFreeSlot()
@@ -119,7 +120,12 @@ namespace Gra
             {
                 getFreeSlot().Tag = img;
                 getFreeSlot().Tag = img;
+                getFreeSlot().Tag = img;
             }
+
+            specialPictureBox = getFreeSlot();
+            specialPictureBox.Tag = "osobny";
+            specialPictureBox.Image = Properties.Resources._6;
         }
 
         private void CLICKTIMER_TICK(object sender, EventArgs e)
@@ -127,14 +133,23 @@ namespace Gra
             HideImages();
             allowClick = true;
             clickTimer.Stop();
+            ResetGuesses();
         }
 
-
-
+        private void SPECIALTIMER_TICK(object sender, EventArgs e)
+        {
+            var specialPic = pictureBoxes.FirstOrDefault(p => p.Tag != null && p.Tag.ToString() == "osobny");
+            if (specialPic != null)
+            {
+                specialPic.Visible = false;
+            }
+            specialTimer.Stop();
+        }
 
         private void wyjdz_Click(object sender, EventArgs e)
         {
             clickTimer.Stop();
+            specialTimer.Stop();
             timer.Stop();
             this.Close();
         }
@@ -147,6 +162,8 @@ namespace Gra
             startGameTimer();
             clickTimer.Interval = 1000;
             clickTimer.Tick += CLICKTIMER_TICK;
+            specialTimer.Interval = 1000;
+            specialTimer.Tick += SPECIALTIMER_TICK;
             startm.Enabled = false;
         }
 
@@ -154,35 +171,59 @@ namespace Gra
         {
             if (!allowClick) return;
             var pic = (PictureBox)sender;
+
+            if (pic.Tag != null && pic.Tag.ToString() == "osobny")
+            {
+                pic.Image = Properties.Resources._6;
+                specialTimer.Start();
+                return;
+            }
+
             if (firstGuess == null)
             {
                 firstGuess = pic;
                 pic.Image = (Image)pic.Tag;
                 return;
             }
-            pic.Image = (Image)pic.Tag;
-            if (pic.Image == firstGuess.Image && pic != firstGuess)
-            {
-                pic.Visible = firstGuess.Visible = false;
-                {
-                    firstGuess = pic;
-                }
-                HideImages();
-            }
-            else { allowClick = false; clickTimer.Start(); }
-            firstGuess = null;
-            if (pictureBoxes.Any(p => p.Visible)) return;
-            MessageBox.Show("Gratulacje");
-            //ZAPIS WYNIKU
-            parent.SCORE = 1;
-            parent.RODZAJ = "Memory";
-            parent.Zapis_do_CSV();
-            ////////////////////////
-            ResetImages();
 
+            if (secondGuess == null)
+            {
+                secondGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            thirdGuess = pic;
+            pic.Image = (Image)pic.Tag;
+
+            if (firstGuess.Tag == secondGuess.Tag && secondGuess.Tag == thirdGuess.Tag && firstGuess != secondGuess && secondGuess != thirdGuess && firstGuess != thirdGuess)
+            {
+                firstGuess.Visible = false;
+                secondGuess.Visible = false;
+                thirdGuess.Visible = false;
+                ResetGuesses();
+
+                if (!pictureBoxes.Any(p => p.Visible))
+                {
+                    MessageBox.Show("Gratulacje!");
+                    parent.SCORE = 1;
+                    parent.RODZAJ = "Memory";
+                    parent.Zapis_do_CSV();
+                    ResetImages();
+                }
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
         }
 
-
+        private void ResetGuesses()
+        {
+            firstGuess = null;
+            secondGuess = null;
+            thirdGuess = null;
+        }
     }
 }
-
